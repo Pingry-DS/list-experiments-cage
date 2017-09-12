9d606b3bbfd10fbb899c205045a4ddfd9564b403
ethanmchung <echung2018@pingry.org>
2017-09-12 00:59:44 -0400

    started sortedinsert but still needs a lot of work

--- Experiments.java
+++ Experiments.java
@@ -221,6 +221,12 @@ public class Experiments implements Comparator<String>
    */
   public static <T>  List<T> SortedInsert(List<T> items, Comparator <T> c) 
   {
+  	List l = new ArrayList<T>();
+  	for (int i = 0; i < items.size(); i++)
+  	{
+  		compare()
+  	}
+  	
   	return l;
   } 
 
